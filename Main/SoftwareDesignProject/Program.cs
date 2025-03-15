df4a8d35c874051dc0f6f6e15a7de665cc53b4d9
cutehusky <liyihong102@gmail.com>
2025-03-15 09:37:29 +0700

    Initial commit

---
+++ Main/SoftwareDesignProject/Program.cs
@@ -0,0 +1,40 @@
+using MudBlazor.Services;
+using SoftwareDesignProject.Client.Pages;
+using SoftwareDesignProject.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add MudBlazor services
+builder.Services.AddMudServices();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(SoftwareDesignProject.Client._Imports).Assembly);
+
+app.Run();
