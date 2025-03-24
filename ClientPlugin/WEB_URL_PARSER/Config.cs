eb2b1d6b2dc07be8178f5cfc341ed997adc9b4c0
LegendAVRS <61302586+LegendAVRS@users.noreply.github.com>
2025-03-24 19:00:56 +0700

    feat: added web url parser plugin

---
+++ ClientPlugin/WEB_URL_PARSER/Config.cs
@@ -0,0 +1,14 @@
+﻿using ClientPluginTemplate;
+
+namespace WEB_URL_PARSER;
+
+public class Config : IConfig
+{
+    public string ID => "26cfe2fd-d31a-4f39-8d85-69386b1736df";
+    public Type EntryPoint => typeof(MainComponent);
+    public static string apiEndPoint;
+    public string APIEndPoint
+    {
+        set => apiEndPoint = value;
+    }
+}
