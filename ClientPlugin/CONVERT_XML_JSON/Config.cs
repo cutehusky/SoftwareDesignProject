e5244d5671f233813afe7af9a5ccb13d1b6d8723
cutehusky <liyihong102@gmail.com>
2025-04-13 21:47:55 +0700

    feat: base64 plugin, jwt parser plugin, xml json plugin

---
+++ ClientPlugin/CONVERT_XML_JSON/Config.cs
@@ -0,0 +1,15 @@
+﻿using ClientPluginTemplate;
+
+namespace CONVERT_XML_JSON;
+
+
+public class Config: IConfig
+{
+    public string ID => "52990b50-978c-44d7-9c49-8832ff2f9be9";
+    public Type EntryPoint => typeof(MainComponent);
+    public static string apiEndPoint;
+    public string APIEndPoint
+    {
+        set => apiEndPoint = value;
+    }
+}
---
+++ ClientPlugin/JWT_PARSER/Config.cs
@@ -0,0 +1,15 @@
+﻿using ClientPluginTemplate;
+
+namespace JWT_PARSER;
+
+
+public class Config: IConfig
+{
+    public string ID => "dda68063-f056-444f-bc0e-37083517ba98";
+    public Type EntryPoint => typeof(MainComponent);
+    public static string apiEndPoint;
+    public string APIEndPoint
+    {
+        set => apiEndPoint = value;
+    }
+}
