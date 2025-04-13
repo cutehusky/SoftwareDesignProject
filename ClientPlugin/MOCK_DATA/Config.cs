f94ca6dab1b83cea7094327d9ea78cfec3f036a6
cutehusky <liyihong102@gmail.com>
2025-04-13 12:43:22 +0700

    feat: mock data plugin

---
+++ ClientPlugin/MOCK_DATA/Config.cs
@@ -0,0 +1,14 @@
+﻿using ClientPluginTemplate;
+
+namespace MOCK_DATA;
+
+public class Config : IConfig
+{
+    public string ID => "1ba34c03-382c-4a7f-a7da-09de2544ae0f";
+    public Type EntryPoint => typeof(MainComponent);
+    public static string apiEndPoint;
+    public string APIEndPoint
+    {
+        set => apiEndPoint = value;
+    }
+}
