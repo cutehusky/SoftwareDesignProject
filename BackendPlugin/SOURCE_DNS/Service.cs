f5ad94d448a5926ef0015f978a1bdb0a579fd03a
LegendAVRS <61302586+LegendAVRS@users.noreply.github.com>
2025-04-14 10:24:21 +0700

    chore: move logic from backend to client side for client plugins

--- BackendPlugin/SOURCE_DNS/Service.cs
+++ BackendPlugin/SOURCE_DNS/Service.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net;
+using System.Net.Sockets;
 
 namespace SOURCE_DNS
 {
@@ -6,8 +7,36 @@ namespace SOURCE_DNS
     {
         public async Task<List<string>> ResolveDns(string value)
         {
-            var addresses = await Dns.GetHostAddressesAsync(value);
-            return addresses.Select(ip => ip.ToString()).ToList();
+            string host;
+
+            // Try to parse value as a URI.
+            if (Uri.TryCreate(value, UriKind.Absolute, out var uriResult))
+            {
+                host = uriResult.Host;
+            }
+            else
+            {
+                host = value;
+            }
+
+            try
+            {
+                var addresses = await Dns.GetHostAddressesAsync(host);
+                return addresses.Select(ip => ip.ToString()).ToList();
+
+            }
+            catch (SocketException ex)
+            {
+                // Handle the exception as needed
+                Console.WriteLine($"Error resolving DNS for {host}: {ex.Message}");
+                return new List<string>();
+            }
+            catch (Exception ex)
+            {
+                // Handle other exceptions as needed
+                Console.WriteLine($"Unexpected error: {ex.Message}");
+                return new List<string>();
+            }
         }
     }
 }
